bf510f1b0c652f28fa5c943d562e2b0f8226c5e9
Abdul Rafi <arafi15@gmail.com>
2022-12-17 17:11:29 +0800

    added totalquantitymodel

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,2 +1,4 @@
 ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
 ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
+
+ForestLiana.custom_actions = [{  name: 'Total Quantities',  verb: 'GET',  path: '/total-quantities',  handler: 'TotalQuantity.total_quantities'}]
---
+++ db/migrate/20221217084030_create_total_quantities.rb
@@ -0,0 +1,10 @@
+class CreateTotalQuantities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :total_quantities do |t|
+      t.references :product, null: false, foreign_key: true
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
