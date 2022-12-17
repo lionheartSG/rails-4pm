be146431242d6185ba6fce151eb7335a9b6982be
Abdul Rafi <arafi15@gmail.com>
2022-12-17 17:31:37 +0800

    removeForestLiana config

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,4 +1,2 @@
 ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
 ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
-
-ForestLiana.actions = [{  name: 'Total Quantities',  verb: 'GET',  path: '/total-quantities',  handler: 'TotalQuantity.total_quantities'}]
